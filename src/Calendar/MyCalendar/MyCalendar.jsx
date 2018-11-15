b062ea1fb8064a5230141d9a40131169f0bb846e
Kamesas <semakaleksandr2014@gmail.com>
2018-11-15 20:39:37 +0200

    saved to local storage

--- src/Calendar/MyCalendar/MyCalendar.jsx
+++ src/Calendar/MyCalendar/MyCalendar.jsx
@@ -24,21 +24,33 @@ class MyCalendar extends Component {
   }
 
   newNote = newNote => {
-    /*  this.setState({ notes: [...newNote] }); */
-    /*  this.setState({ notes: this.state.note.push[newNote] }); */
     this.setState({ notes: [newNote, ...this.state.notes] });
-    console.log("testNotes ---", this.state.notes);
-    console.log("newNote --- ", newNote);
   };
 
-  closeModal = () => {
-    this.setState({ addNoteWindow: false });
+  saveToLocalStorage = () => {
+    const notes = JSON.stringify(this.state.notes);
+    localStorage.setItem("notes", notes);
   };
 
   componentDidMount() {
     this.widthHeigth();
     this.onResize();
+
+    const savedNotes = JSON.parse(localStorage.getItem("notes"));
+    if (savedNotes) {
+      this.setState({ notes: savedNotes });
+    }
   }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.notes !== this.state.notes) {
+      this.saveToLocalStorage();
+    }
+  }
+
+  closeModal = () => {
+    this.setState({ addNoteWindow: false });
+  };
 
   widthHeigth = () => {
     this.setState({ widthDay: this.dayDivRef.current.clientWidth });
